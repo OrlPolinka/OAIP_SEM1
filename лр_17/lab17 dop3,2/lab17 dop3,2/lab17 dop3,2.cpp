fd48223aa87f27afe7efc65944d3014e09dc7df9
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-22 00:02:28 +0300

    Add files via upload

---
+++ лр_17/lab17 dop3,2/lab17 dop3,2/lab17 dop3,2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream> 
+using namespace std;
+
+int check(bool& hasNegative, int n, int i, int** matrix) {
+	for (int j = 0; j < n; j++) {
+		if (matrix[i][j] < 0) {
+			hasNegative = true;
+			break;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	
+	//вводим размер матрицы
+	cout << "Введите размеры матрицы: ";
+	cin >> n;
+
+	//Динамическое выделение памяти для матрицы
+	int** matrix = new int* [n];
+	for (int i = 0; i < n; i++) {
+		matrix[i] = new int[n];
+	}
+
+	//Вводим элементы матрицы
+	cout << "Введите элементы матрицы: " << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+
+	//Поиск строк без отрицательных элементов и вычисление их суммы
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		bool hasNegative = false;
+		check(hasNegative, n, i, matrix);
+	
+
+	    //Если отрицаьельных элементов нет, то добавляем сумму элементов строки к общей сумме
+	    if (!hasNegative) {
+			for (int j = 0; j < n; j++) {
+				sum += matrix[i][j];
+			}
+	    }
+    }
+
+	//выводим результат на экран
+	cout << "Сумма элементов строк без отрицательных элементов: " << sum << endl;
+
+	//Освобождаем память, выделенную для м матрицы
+	for (int i = 0; i < n; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+
+	return 0;
+}
---
+++ лр_17/lab17,2/lab17,2/lab17,2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream> 
+using namespace std;
+
+//Функция для поиска максимального числа в матрице
+int findMax(int** a, int n, int m, int& max, int& maxN, int& maxM, int& count) {
+    max = a[0][0];
+    count = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] > max) {
+                max = a[i][j];
+                maxN = i;
+                maxM = j;
+                count = 1;
+            }
+            else {
+                if (a[i][j] == max) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+    int n, m;
+    //Вводим размер матрицы
+    cout << "Введите размеры матрицы: ";
+    cin >> n >> m;
+
+    //Создаем массив и выделяем для него память 
+    int** a;
+    a = new int* [n];
+    for (int i = 0; i < n; i++) {
+        a[i] = new int[m];
+    }
+
+    //Выводим нашу матрицу на экран
+    cout << "Введите элементы матрицы: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    //вводим переменные и вызываем функцию
+    int max, maxN, maxM, count;
+    findMax(a, n, m, max, maxN, maxM, count);
+
+    //Выводим результат на экран в заависимости от условия
+    if (count > 1) {
+        cout << "Максимальное число: " << max << endl;
+        cout << "Индексы: [" << maxN << "] [" << maxM << "]" << endl;
+        cout << "Количество повторений: " << count << endl;
+    }
+    else {
+        cout << "Максимальное число не повторяется." << endl;
+    }
+
+    //Освобождаем память, выделенную под массив
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+}
