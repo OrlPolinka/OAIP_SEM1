6e77230e642667c69a665ce816048bf561b97d7b
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-21 23:31:40 +0300

    Add files via upload

---
+++ лр_17/lab17,1/lab17,1/lab17,1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+using namespace std;
+
+//Функция для нахождение четных элементров массива на нечетных местах
+int findElements(int* a, int n) {
+	cout << "Номера четных элементов, стоящих на нечетных местах: ";
+	for (int i = 0; i < n; i += 2) {
+		if (a[i] % 2 == 0) {
+			cout << i + 1 << " ";
+		}
+	}
+	return 0;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "Введите размер массива n = ";
+	cin >> n;//вводим размер массива
+	cout << endl;
+
+	int* a = new int[n];//создаём динамический массив
+
+	//Вводим массив
+	cout << "Введите элементы массива: ";
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	findElements(a, n);//вызываем функцию
+
+	delete[] a;//осовбождаем память, выделенную под массив
+	return 0;
+}
---
+++ лр_18/lab18,1/lab18,1/lab18,1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    // Открываем файл для чтения
+    FILE* fileA;
+    if (fopen_s(&fileA, "fileA.txt", "r") != 0) {
+        cerr << "Ошибка открытия файла fileA.txt" << endl;
+        return 1;
+    }
+
+    //вводим число x
+    int x;
+    cout << "Введите число x: ";
+    cin >> x;
+
+    //Читаем компоненты последовательности из файла
+    int n = 12;
+
+    //создаем массив для хранения компонентов последовательности
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        if (fscanf_s(fileA, "%d", &arr[i]) != 1) {
+            cerr << "Ошибка чтения элемента файла fileA.txt" << endl;
+            delete[] arr;
+            fclose(fileA);
+            return 1;
+        }
+    }
+    fclose(fileA);
+
+    //Ищем 2 числа, среднее арифмитическое которых ближк всего к x
+    int closestSum = INT_MAX;
+    int num1 = 0, num2 = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int sum = (arr[i] + arr[j]) / 2;
+            if (abs(sum - x) < abs(closestSum - x)) {
+                closestSum = sum;
+                num1 = arr[i];
+                num2 = arr[j];
+            }
+        }
+    }
+
+    //выводим результат на экран
+    cout << "Два числа из последовательности, среднее арифмитическое которых ближе всего к x: " << num1 << ", " << num2 << endl;
+    delete[] arr;
+    return 0;
+}
