acb6630fbbec0f1d4c5ef7d93eca9db4bc9d8133
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-21 23:56:02 +0300

    Add files via upload

---
+++ лр_16/lab16 dop1,1/lab16 dop1,1/lab16 dop1,1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+//Функция для нахождения столбца, в котором находится элемент d
+int find(int** a, int x) {
+    for (int i = 0; i < sizeof(a[0]); i++) {
+        for (int j = 0; j < sizeof(a[0][0]); j++) {
+            if (a[i][j] == x) {
+                return i + 1;
+            }
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int N, M, d;
+    //Вводим размер матрицы
+    cout << "Введите размеры матрицы: ";
+    cin >> N >> M;
+    //Вводим число k
+    cout << "Введите число d = ";
+    cin >> d;
+
+    //Создаем массив и выделяем для него память 
+    int** a;
+    a = new int* [N];
+    for (int i = 0; i < N; i++) {
+        a[i] = new int[M];
+    }
+
+    //Выводим нашу матрицу на экран
+    cout << "Введите элементы матрицы: " << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    //Выводим результат на экран, используя функцию
+    cout << "Строка, в которой содержится элемент d: " << find(a, d) << endl;
+
+    //освобождаем выделенную память
+    for (int i = 0; i < N; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+}
---
+++ лр_16/lab16 dop2,1/lab16 dop2,1/lab16 dop2,1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+//Функция для нахождения количества положительных, отрицательных и равных нулю чисел
+void findElementsCount(int& negativeCount, int& positiveCount, int& zeroCount, int** a, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (a[i][j] < 0) {
+                negativeCount++;
+            }
+            else {
+                if (a[i][j] > 0) {
+                    positiveCount++;
+                }
+                else {
+                    zeroCount++;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int N, M;
+    //Вводим размер матрицы
+    cout << "Введите размеры матрицы: ";
+    cin >> N >> M;
+
+    //Создаем массив и выделяем для него память 
+    int** a;
+    a = new int* [N];
+    for (int i = 0; i < N; i++) {
+        a[i] = new int[M];
+    }
+
+    //Выводим нашу матрицу на экран
+    cout << "Введите элементы матрицы: " << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    //Вводим переменные и используем функцию
+    int negativeCount = 0;
+    int positiveCount = 0;
+    int zeroCount = 0;
+    findElementsCount(negativeCount, positiveCount, zeroCount, a, N, M);
+    //Выводим результат на экран
+    cout << "Отрицательные: " << negativeCount << endl;
+    cout << "Положительные: " << positiveCount << endl;
+    cout << "Нулевые: " << zeroCount << endl;
+
+    //освобождаем выделенную память
+    for (int i = 0; i < N; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+}
