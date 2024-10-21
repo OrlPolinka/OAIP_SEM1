ad5e01fc6c32dfef010b51c00afa3c08aa40dbd6
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-21 23:18:22 +0300

    Add files via upload

---
+++ lab8,3/lab8,3/lab8,3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	float sum = 0, d = 12.5e-4, h, a;
+	int i;
+	const int n = 5;
+	for (i = 1; i <= n; i++)//Выполняем n раз, для всех значений a 
+	{
+		cout << "Введите a" << i << endl;
+		cin >> a;//вводим a
+		sum = sum + pow(a, 2);//рассчитываем сумму для каждого значения a
+	}
+	cout << "Сумма равна " << sum << endl;//выводим значение суммы на экран
+	h = d + sum;//рассчитываем h
+	cout << "h = " << h << endl;//выводим значение h на экран
+	
+	return 0;
+}
