77cb1b561eb145ea98bb9b94e5490e1960fe0400
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-21 23:16:25 +0300

    Add files via upload

---
+++ lab8 dop1/lab8 dop1/lab8 dop1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int i, n, sum = 0, x;
+	cout << "n = ";
+	cin >> n;//вводим n
+	for (i = 1; i <= n; i++)//Выполняем n раз, для всех значений x 
+	{
+		cout << "Введите x" << i << endl;
+		cin >> x;//вводим x
+
+		if (x % 2 == 0)//если х кратно 2(четное), то
+		{
+			sum = sum + x;//к сумме добавляем данное значение х
+		}
+		
+	}
+	cout << "Сумма равна " << sum << endl;//выводим значение суммы на экран
+	return 0;
+}
---
+++ lab8/lab8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	float q = 0, x, y;
+	int i;
+	const int n = 6;
+	for (i = 1; i <= n; i++)//Выполняем 6 раз, для всех значений x и y
+	{
+		cout << "Введите x" << i << endl;
+		cin >> x;//вводим x
+		cout << "Введите y" << i << endl;
+		cin >> y;//вводим y
+		q = q + (x * y);//Рассчитываем нашу сумму для всех каждого значения x и y
+	}
+	cout << "Ответ: " << q << endl;//выводим ответ на экран
+	return 0;
+}
